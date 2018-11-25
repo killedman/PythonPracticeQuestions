a2967b005853a8c302b3470952f25c66be728f31
killedman <killedmanbylonely@gmail.com>
2018-11-25 09:30:01 +0800

    第 0014 题：纯文本文件 student.txt为学生信息, 里面的内容（包括花括号），请将上述内容写到 student.xls 文件中

---
+++ write_student_data_to_excel.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf8 -*-
+
+# author: dreampython
+# date: 2018-10-23
+# 第 0014 题： 纯文本文件 student.txt为学生信息, 里面的内容（包括花括号）如下所示：
+# 请将上述内容写到 student.xls 文件中
+
+from openpyxl import Workbook
+import json
+
+def read_txt_file():
+    with open('./student.txt','r',encoding='utf-8-sig') as file:
+        load_dict = json.load(file)
+    return load_dict
+
+def create_excel_file(students):
+    wb = Workbook()
+    sheet = wb.active
+    sheet.title = "student"
+    i = 0
+    for student in students:
+        i = i + 1
+        sheet["A%d" %i].value = student
+        sheet["B%d" %i].value,sheet["C%d" %i].value, sheet["D%d" %i].value, \
+            sheet["E%d" %i].value = students.get(student)
+    wb.save('./stduents.xlsx')
+
+if __name__ == "__main__":
+    student_dict = read_txt_file()
+    create_excel_file(student_dict) 
